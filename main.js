661d62b3ba0f1a0deb2307bef31c2ef7bab4244c
Webster Grimes <cwebsterg@mac.com>
2022-09-20 19:57:58 -0400

    loading of calculator code pre-final edits

---
+++ main.js
@@ -0,0 +1,53 @@
+// let squares = document.querySelectorAll(".square");
+// console.log(squares)
+// JS, go find all the elements with the class 'square'
+// querySelector only finds the first element, All expands
+// JS gets the literal chunks of HTML
+
+// for (let square of squares) {
+  // use a loop to apply the behavior to every square
+
+  // square.addEventListener("click", (event) => {
+    // listen (wait) for the user to click and then do the stuff inside the {}
+    // template literal allows us to put variable values in the middle of text
+    // square.classList.remove("purple");
+    // square.classList.add("teal");
+    // if (square.id === "square--one") {
+    //   console.log(`The ${square.innerText} was clicked`);
+    //   frame.style.backgroundImage = 'url("http://placekitten.com/300/300")';
+  //   }
+  // });
+// }
+
+const display = document.querySelector(".display")
+//consider all of the .squares (class) on page
+// let squares = document.querySelectorAll(".square")
+// for (let input of squares)
+// // event listener when we click WHAT did we click?
+// input.addEventListener("click", (event) => {
+//   console.log(input.innerText)
+//   let newElement = document.createElement("div")
+//   let text = document.createTextNode(event.target.innerText)
+//   newElement.appendChild(text)
+//   display.appendChild(newElement)
+// })
+
+let squares = Array.from(document.getElementsByClassName("square"));
+// declaring the name of new array containing all squares (operators and operands)
+squares.map((square) => {
+  square.addEventListener("click", (e) => {
+    switch (square.innerText) {
+      case "C":
+        display.innerText = "";
+        break;
+      case "=":
+        display.innerText = eval(display.innerText);
+        break;
+      default:
+        display.innerText += square.innerText;
+    }
+  })
+}
+)
+
+
